18b4749d056d5d7a21f25fa25af7dcae97bb50e1
nicole813 <64963568+nicole813@users.noreply.github.com>
2021-04-19 16:39:36 +0800

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,6 @@
+#define SECRET_SSID "AP-401-2.4G"    // replace MySSID with your WiFi network name
+#define SECRET_PASS "class401"  // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1362990     // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "ZO6D742B6EIJXD3U"   // replace XYZ with your channel write API Key
+#define SECRET_READ_APIKEY "4JKA1RZ733OHTCXR"
